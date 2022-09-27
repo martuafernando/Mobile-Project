fe273b59919b52f8679883c36eb164701a716a9b
martuafernando <fernandosibarani45@gmail.com>
2022-09-27 16:48:13 +0700

    Do refactor

--- app/src/main/java/com/example/projectmobile/RegisterFragment.java
+++ app/src/main/java/com/example/projectmobile/RegisterFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.projectmobile.databinding.FragmentFirstBinding;
+import com.example.projectmobile.databinding.FragmentRegisterBinding;
 
-public class FirstFragment extends Fragment {
+public class RegisterFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private FragmentRegisterBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,11 +29,11 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(RegisterFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
