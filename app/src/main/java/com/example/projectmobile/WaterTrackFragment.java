53d9d19c86c411e93900465b7fbd000751e00d01
martuafernando <fernandosibarani45@gmail.com>
2022-09-28 15:28:50 +0700

    add feature add task

--- app/src/main/java/com/example/projectmobile/WaterTrackFragment.java
+++ app/src/main/java/com/example/projectmobile/WaterTrackFragment.java
@@ -1,64 +1,58 @@
 package com.example.projectmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WaterTrackFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.projectmobile.databinding.FragmentWaterTrackBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WaterTrackFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentWaterTrackBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WaterTrackFragment() {
-        // Required empty public constructor
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentWaterTrackBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WaterTrackFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WaterTrackFragment newInstance(String param1, String param2) {
-        WaterTrackFragment fragment = new WaterTrackFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    String[] ListElements = new String[] {};
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final List<String> ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (getActivity(), android.R.layout.simple_list_item_1, ListElementsArrayList);
+        binding.containerListView.setAdapter(adapter);
+
+        binding.addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListElementsArrayList.add(binding.inputTaskEditText.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_water_track, container, false);
-    }
 }
